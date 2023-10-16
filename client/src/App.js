1f7a1906e9e4337ca70c537e60a82b397b92dc5b
CalebSullivan35 <CalebSullivan357@gmail.com>
2023-10-16 10:06:13 -0500

    intial project setup

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { tryGetLoggedInUser } from "./managers/authManager";
+import { Spinner } from "reactstrap";
+import ApplicationViews from "./components/ApplicationViews";
+import NavBar from "./components/navbar";
+
+function App() {
+ const [loggedInUser, setLoggedInUser] = useState();
+
+ useEffect(() => {
+  // user will be null if not authenticated
+  tryGetLoggedInUser().then((user) => {
+   setLoggedInUser(user);
+  });
+ }, []);
+
+ // wait to get a definite logged-in state before rendering
+ if (loggedInUser === undefined) {
+  return <Spinner />;
+ }
+
+ return (
+  <>
+   <NavBar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
+   <ApplicationViews
+    loggedInUser={loggedInUser}
+    setLoggedInUser={setLoggedInUser}
+   />
+  </>
+ );
+}
+
+export default App;
