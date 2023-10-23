a530ce9e62df43f5ae057e8c69926da82504b557
CalebSullivan35 <CalebSullivan357@gmail.com>
2023-10-23 15:23:17 -0500

    Dropdown For Favorites

--- client/src/components/FavoritesList/FavoritesList.js
+++ client/src/components/FavoritesList/FavoritesList.js
@@ -7,29 +7,85 @@ import { useNavigate } from "react-router-dom";
 import { GrSubtractCircle } from "react-icons/gr";
 
 export const FavoritesList = ({ loggedInUser }) => {
+ const genres = [
+  "Action",
+  "Adventure",
+  "Animation",
+  "Comedy",
+  "Crime",
+  "Documentary",
+  "Drama",
+  "Family",
+  "Fantasy",
+  "History",
+  "Horror",
+  "Music",
+  "Mystery",
+  "Romance",
+  "Science Fiction",
+  "TV Movie",
+  "Thriller",
+  "War",
+  "Western",
+ ];
  const [myFavorites, setMyFavorites] = useState([]);
+ const [selectedGenre, setSelectedGenre] = useState(null);
+ const [moviesToDisplay, setMoviesToDisplay] = useState([]);
+
  const navigate = useNavigate();
  function getData() {
   getUserProfileMovieByUserId(loggedInUser.id).then(setMyFavorites);
  }
-
  function handleDeleteButton(id) {
   deleteUserProfileMovie(id).then(() => getData());
  }
-
  useEffect(() => {
   getData();
  }, []);
+ //useEffect to intially set movies to display as all favorites.
+ useEffect(() => {
+  setMoviesToDisplay(myFavorites);
+ }, [myFavorites]);
+
+ //function that will set state dependending on dropdown
+ async function handleDropDownChanges(e) {
+  //first case will be that if the value ever goes back to none then set it to default list.
+  if (e.target.value == "None") {
+   setMoviesToDisplay(myFavorites);
+   return;
+  }
+  setMoviesToDisplay(
+   myFavorites.filter((f) => {
+    return f.movie.genres.some((genre) => genre.name === e.target.value);
+   })
+  );
+ }
 
  if (myFavorites.length < 1) {
   return "";
  }
 
  return (
-  <div className="w-screen h-screen">
+  <div className="h-screen">
    <h1>Favorites: </h1>
+   <div className="flex w-11/12 justify-end">
+    <select
+     className="select w-full max-w-xs select-accent"
+     onChange={(e) => {
+      handleDropDownChanges(e);
+     }}
+    >
+     <option disabled selected>
+      Select Genre
+     </option>
+     <option value="None">Do Not Filter By Genere</option>
+     {genres.map((g) => {
+      return <option value={g}>{g}</option>;
+     })}
+    </select>
+   </div>
    <div className="flex flex-row p-5 m-5">
-    {myFavorites.map((f) => {
+    {moviesToDisplay.map((f) => {
      return (
       <div className=" relative">
        <img
